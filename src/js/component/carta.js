0e01a8b6fae99ceb035ead340a7ff23cd593c75c
Maryvi <mcamachog@hotmail.com>
2022-11-07 03:54:52 +0000

    estilos grises completos

--- src/js/component/carta.js
+++ src/js/component/carta.js
@@ -2,10 +2,19 @@ import React from "react";
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import '../../styles/carta.css'
+import '../../styles/index.css'
 
 function Carta() {
+ 
+  const mystyle = {
+    width: "18rem",
+    padding: "5px",
+    border: "0px",
+  };
+
   return (
-    <Card style={{ width: '18rem' }}>
+    
+    <Card style={mystyle}>
       <div className="contenedor-gris-imagen">
         500 x 325
       </div>
@@ -18,8 +27,8 @@ function Carta() {
           Some quick example text to build on the card title and make up the
           bulk of the card's content.
         </Card.Text>
-        <div className="contenedor-boton-carta">
-          <Button variant="primary">Go somewhere</Button>
+        <div className="contenedor-boton-centrado">
+          <Button variant="primary">Find Out More!</Button>
         </div>
    
       </Card.Body>
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -3,6 +3,8 @@ import BarraDeNavegacion from './navbar'
 import Carta from './carta'
 import Button from 'react-bootstrap/Button';
 import '../../styles/home.css'
+import '../../styles/index.css'
+
 //include images into your bundle
 
 
@@ -13,9 +15,11 @@ const Home = () => {
 			<BarraDeNavegacion />
 			<div className="contenedor-principal">
 				<div className="contenedor-jumbotron">
-					<h1>Titulo</h1>
-					<p>Parrafo</p>
-					<Button variant="primary">Call to action!</Button>
+					<h1>A Warm Welcome!</h1>
+					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium voluptate illo illum laborum. Perspiciatis provident laboriosam tempora distinctio porro vitae doloribus at modi, et alias saepe quas labore fugit ea!</p>
+					<div className="contenedor-boton-izquierda">					
+						<Button variant="primary">Call to action!</Button>
+					</div>
 				</div>
 				<div className="contenedor-cartas">
 					<Carta />
@@ -25,7 +29,7 @@ const Home = () => {
 				</div>
 			</div>
 			<div className="contenedor-pie-de-pagina">
-				footer
+				Copyright &copy; Your Web site 2022
 			</div>
 		</div>
 	);
